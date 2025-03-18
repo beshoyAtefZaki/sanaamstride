ea6b7d2334d4793817b5d811002a082bd1d8a9e0
42Group <42group@42Groups-MacBook-Pro.local>
2025-03-18 23:29:52 +0200

    update project doctype

--- sanaamstride/sanaamstride/doctype/project/project.js
+++ sanaamstride/sanaamstride/doctype/project/project.js
@@ -2,9 +2,99 @@
 // For license information, please see license.txt
 
 frappe.ui.form.on("Project", {
+    create_project_dialog(frm){
+
+        // Create new child project with current project as parent
+        let d = new frappe.ui.Dialog({
+            title: 'Enter details',
+            fields: [
+                {
+                    label: 'Project Name',
+                    fieldname: 'project_name',
+                    fieldtype: 'Data',
+                    reqd :1
+                },
+                {
+                    label: 'Is Parent',
+                    fieldname: 'is_parent',
+                    fieldtype: 'Check',
+                    default: 0,
+                    read_only: 1
+                },
+                {
+                    label: 'Parent Project',
+                    fieldname: 'parent_project',
+                    fieldtype: 'Link' ,
+                    options : 'Project',
+                    read_only: 1,
+                    default: frm.doc.name,
+
+                    get_query: function() {
+                        return {
+                            filters: {
+                                is_parent: 1
+                            }
+                        };
+                    }
+                },
+                {
+                    label: 'Description',
+                    fieldname: 'description',
+                    fieldtype: 'Small Text',
+                    reqd :1
+                },
+            ],
+            size: 'small', // small, large, extra-large 
+            primary_action_label: 'Submit',
+            primary_action(values) {
+              frappe.call({
+                method: 'sanaamstride.sanaamstride.doctype.project.project.create_sup_project',
+                args: {
+                    current_project: frm.doc.name,
+                    project_name: values.project_name,
+                    parent_project: frm.doc.name,
+                    is_parent: 0,
+                    description: values.description
+                },
+                callback: function(r) {
+                    if (r.message) {
+                        frappe.show_alert({
+                           
+                            message: __(`Project created successfully ${r.message}`),
+                            indicator: 'green'
+                        });
+                        d.hide();
+                        frappe.set_route('Form', 'Project', r.message);
+                    }
+                }
+
+              })
+            }
+        });
+        
+        d.show();
+
+    },
+    apply_filter(frm){
+        frm.set_query("parent_project", function() {
+            return {
+                filters: {
+                    is_parent: 1
+                }
+            };
+        });
+    
+
+    },
 	refresh(frm) {
         // Show/hide buttons based on is_parent value
+        // frm.events.apply_filter(frm);
         if (frm.doc.is_parent) {
+            frm.add_custom_button(__("Create Project"), function() {
+               frm.events.create_project_dialog(frm);
+            },__("Utilities"));
+        }
+        if (frm.doc.is_parent == false) {
             frm.add_custom_button(__("Create Task"), function() {
                 frappe.new_doc("Task", {
                     project: frm.doc.name
@@ -24,12 +114,12 @@ frappe.ui.form.on("Project", {
         });
     },
     
-    is_parent(frm) {
-        // Show/hide parent_project field based on is_parent value
-        frm.toggle_display('parent_project', !frm.doc.is_parent);
-        // Refresh to update button visibility
-        frm.refresh();
-    },
+    // is_parent(frm) {
+    //     // Show/hide parent_project field based on is_parent value
+    //     frm.toggle_display('parent_project', !frm.doc.is_parent);
+    //     // Refresh to update button visibility
+    //     frm.refresh();
+    // },
     
     status(frm) {
         if (frm.doc.status === "Completed") {
--- sanaamstride/sanaamstride/doctype/project/project.py
+++ sanaamstride/sanaamstride/doctype/project/project.py
@@ -1,9 +1,36 @@
 # Copyright (c) 2025, beshoy atef and contributors
 # For license information, please see license.txt
 
-# import frappe
+import frappe
 from frappe.model.document import Document
 
 
 class Project(Document):
-	pass
+	@property
+	def total_sup_projects(self):
+		return (frappe.db.count("Project", filters={"parent_project": self.name}))
+	
+	def validate(self):
+		self.validate_parent_project()
+		
+
+	def validate_parent_project(self):
+		if self.parent_project:
+			if frappe.get_value("Project", self.parent_project, "is_parent") == 0:
+				frappe.throw("Parent Project must be a group")
+
+
+
+@frappe.whitelist()
+def create_sup_project( current_project ,project_name, parent_project , is_parent=0 , description=None):
+	if current_project == parent_project:
+		project = frappe.new_doc("Project")
+		project.project_name = project_name
+		project.parent_project = parent_project
+		project.is_parent = is_parent
+		project.description = description
+		project.save()
+		return project.name
+	else:	
+		frappe.throw("You can't create a sub project for another project")
+	
