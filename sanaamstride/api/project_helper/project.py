08b002b0c4c8cacc792f717209a6c06380bd37b9
MBM <mbm@MBMs-MacBook-Pro.local>
2025-04-12 16:22:22 +0300

    vue task

--- sanaamstride/api/project_helper/project.py
+++ sanaamstride/api/project_helper/project.py
@@ -16,12 +16,32 @@ def get_projectList(*args , **kwargs) :
                                                         "project_name" , 
                                                         "status" ]
                                     )
-       
+        
+        # Get sprints and tasks for each child project
+        child_projects_with_details = []
+        for child in child_projects:
+            # Get sprints for this child project
+            sprints = frappe.get_all("Task",
+                                   filters={"project": child.name, "is_sprint": 1},
+                                   fields=["name", "name1 as sprint_name", "status"])
+            
+            # Get tasks for this child project
+            tasks = frappe.get_all("Task",
+                                 filters={"project": child.name, "is_sprint": 0},
+                                 fields=["name", "name1 as task_name", "status"])
+            
+            child_projects_with_details.append({
+                "name": child.name,
+                "project_name": child.project_name,
+                "status": child.status,
+                "sprints": sprints,
+                "tasks": tasks
+            })
+        
         response.append({
-            "parent" : project.name,
-            "parent_name" : project.project_name,
-            "parent_status" : project.status,
-            "child" : child_projects,
-           
+            "name": project.name,
+            "project_name": project.project_name,
+            "status": project.status,
+            "child_projects": child_projects_with_details
         })
     return response
