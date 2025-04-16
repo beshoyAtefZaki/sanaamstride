f1b30a63149495b966d6f3110e3863aa03261818
almufadda <a.almufadda@sanaam.sa>
2025-04-16 10:45:59 +0300

    Task-00006 Add API for create project and child project

--- sanaamstride/api/project.py
+++ sanaamstride/api/project.py
@@ -1,7 +1,18 @@
-import frappe 
-
-from sanaamstride.api.project_helper.project import get_projectList
+import frappe
+from sanaamstride.api.project_helper.project import get_projectList, create_project_service
 
 @frappe.whitelist()
-def get_all(*args , **kwargs) :
-    return get_projectList(*args , **kwargs)
+def get_all(project_name=None, **kwargs):
+    return get_projectList(project_name=project_name, **kwargs)
+
+@frappe.whitelist()
+def create_project(project_name, is_parent=1, parent_project=None, status="Waiting", description=None, **kwargs):
+
+    return create_project_service(
+        project_name=project_name,
+        is_parent=is_parent,
+        parent_project=parent_project,
+        status=status,
+        description=description or "",
+        **kwargs
+    )
